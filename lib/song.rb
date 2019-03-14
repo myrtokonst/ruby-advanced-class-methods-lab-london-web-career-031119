031d338f839eb44b0d0dc0af4fd40c03a3ed0958
Myrto Konstantinidi <myrtokonst@gmail.com>
2019-03-14 16:34:34 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,7 +34,7 @@ def self.find_by_name(name)
 end
 
 def self.find_or_create_by_name(name)
-  if self.name.include?(name)
+  if self.all.name.include?(name)
     song_1 =self.find_by_name(name)
     song_1
   else self.create_by_name(name)
